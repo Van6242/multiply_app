9a7d15358d8b6a735bcf922864cf403f9cb845a4
Van6242 <qhart52336@gmail.com>
2022-03-03 10:32:48 -0500

    multiply app v1

--- multiply/ViewController.swift
+++ multiply/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+var outl = 0
+class ViewController: UIViewController
+{
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet weak var outLable: UILabel!
+    
+    @IBOutlet weak var in1: UITextField!
+    
+    @IBOutlet weak var in2: UITextField!
+    
+    
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
+    
+        
     }
-
+    @IBAction func entbutton(_ sender: Any)
+    {
+        var inp1 = Double(in1.text!) ?? 0
+        
+        var inp2 = Double(in2.text!) ?? 0
+        
+        var apple = inp1 * inp2
+        
+        outLable.text = "\(apple)"
+        
+    }
+    
 
 }
 
